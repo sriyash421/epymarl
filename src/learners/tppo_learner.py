d38f0a177d057ffaf81af4f517e51f8ec1e9fa17
sriyash421 <poddarsriyash@gmail.com>
2023-04-19 03:29:14 +0530

    added topo code

---
+++ src/learners/tppo_learner.py
@@ -0,0 +1,115 @@
+import copy
+from components.episode_buffer import EpisodeBatch
+from modules.critics.coma import COMACritic
+from modules.critics.centralV import CentralVCritic
+from utils.rl_utils import build_td_lambda_targets
+from utils.top_utils import TopologicalRegularizer
+import torch as th
+from torch.optim import Adam
+from modules.critics import REGISTRY as critic_resigtry
+from components.standarize_stream import RunningMeanStd
+from .ppo_learner import PPOLearner
+
+
+class TPPOLearner(PPOLearner):
+    def __init__(self, mac, scheme, logger, args):
+        super().__init__(mac, scheme, logger, args)
+        self.topological_reg = TopologicalRegularizer(args.top_coef, "cuda" if args.use_cuda else "cpu")
+
+    @property
+    def get_agent_models(self):
+        return self.mac.agent.agents
+        
+    def train(self, batch: EpisodeBatch, t_env: int, episode_num: int):
+        # Get the relevant quantities
+
+        rewards = batch["reward"][:, :-1]
+        actions = batch["actions"][:, :]
+        terminated = batch["terminated"][:, :-1].float()
+        mask = batch["filled"][:, :-1].float()
+        mask[:, 1:] = mask[:, 1:] * (1 - terminated[:, :-1])
+        actions = actions[:, :-1]
+        if self.args.standardise_rewards:
+            self.rew_ms.update(rewards)
+            rewards = (rewards - self.rew_ms.mean) / th.sqrt(self.rew_ms.var)
+
+        # No experiences to train on in this minibatch
+        if mask.sum() == 0:
+            self.logger.log_stat("Mask_Sum_Zero", 1, t_env)
+            self.logger.console_logger.error("Actor Critic Learner: mask.sum() == 0 at t_env {}".format(t_env))
+            return
+
+        mask = mask.repeat(1, 1, self.n_agents)
+
+        critic_mask = mask.clone()
+
+        old_mac_out = []
+        self.old_mac.init_hidden(batch.batch_size)
+        for t in range(batch.max_seq_length - 1):
+            agent_outs = self.old_mac.forward(batch, t=t)
+            old_mac_out.append(agent_outs)
+        old_mac_out = th.stack(old_mac_out, dim=1)  # Concat over time
+        old_pi = old_mac_out
+        old_pi[mask == 0] = 1.0
+
+        old_pi_taken = th.gather(old_pi, dim=3, index=actions).squeeze(3)
+        old_log_pi_taken = th.log(old_pi_taken + 1e-10)
+
+        for k in range(self.args.epochs):
+            mac_out = []
+            self.mac.init_hidden(batch.batch_size)
+            for t in range(batch.max_seq_length - 1):
+                agent_outs = self.mac.forward(batch, t=t)
+                mac_out.append(agent_outs)
+            mac_out = th.stack(mac_out, dim=1)  # Concat over time
+
+            pi = mac_out
+            advantages, critic_train_stats = self.train_critic_sequential(self.critic, self.target_critic, batch, rewards,
+                                                                          critic_mask)
+            advantages = advantages.detach()
+            # Calculate policy grad with mask
+
+            pi[mask == 0] = 1.0
+
+            pi_taken = th.gather(pi, dim=3, index=actions).squeeze(3)
+            log_pi_taken = th.log(pi_taken + 1e-10)
+
+            ratios = th.exp(log_pi_taken - old_log_pi_taken.detach())
+            surr1 = ratios * advantages
+            surr2 = th.clamp(ratios, 1 - self.args.eps_clip, 1 + self.args.eps_clip) * advantages
+
+            entropy = -th.sum(pi * th.log(pi + 1e-10), dim=-1)
+            pg_loss = -((th.min(surr1, surr2) + self.args.entropy_coef * entropy) * mask).sum() / mask.sum()
+
+
+            top_loss = self.topological_reg(self.get_agent_models)
+            print("Top Loss", top_loss, pg_loss)
+            pg_loss = top_loss
+
+            # Optimise agents
+            self.agent_optimiser.zero_grad()
+            pg_loss.backward()
+            grad_norm = th.nn.utils.clip_grad_norm_(self.agent_params, self.args.grad_norm_clip)
+            print("Grad norm", grad_norm)
+            self.agent_optimiser.step()
+
+        self.old_mac.load_state(self.mac)
+
+        self.critic_training_steps += 1
+        if self.args.target_update_interval_or_tau > 1 and (
+                self.critic_training_steps - self.last_target_update_step) / self.args.target_update_interval_or_tau >= 1.0:
+            self._update_targets_hard()
+            self.last_target_update_step = self.critic_training_steps
+        elif self.args.target_update_interval_or_tau <= 1.0:
+            self._update_targets_soft(self.args.target_update_interval_or_tau)
+
+        if t_env - self.log_stats_t >= self.args.learner_log_interval:
+            ts_logged = len(critic_train_stats["critic_loss"])
+            for key in ["critic_loss", "critic_grad_norm", "td_error_abs", "q_taken_mean", "target_mean"]:
+                self.logger.log_stat(key, sum(critic_train_stats[key]) / ts_logged, t_env)
+
+            self.logger.log_stat("advantage_mean", (advantages * mask).sum().item() / mask.sum().item(), t_env)
+            self.logger.log_stat("pg_loss", pg_loss.item(), t_env)
+            self.logger.log_stat("agent_grad_norm", grad_norm.item(), t_env)
+            self.logger.log_stat("pi_max", (pi.max(dim=-1)[0] * mask).sum().item() / mask.sum().item(), t_env)
+            self.log_stats_t = t_env
---
+++ src/utils/top_utils.py
@@ -0,0 +1,174 @@
+'''
+Methods for calculating lower-dimensional persistent homology.
+'''
+
+import torch as th
+import itertools
+import numpy as np
+
+class UnionFind:
+    '''
+    An implementation of a Union--Find class. The class performs path
+    compression by default. It uses integers for storing one disjoint
+    set, assuming that vertices are zero-indexed.
+    '''
+
+    def __init__(self, n_vertices, device):
+        '''
+        Initializes an empty Union--Find data structure for a given
+        number of vertices.
+        '''
+
+        self._parent = th.arange(n_vertices, dtype=int, device=device)
+        self.device = device
+
+    def find(self, u):
+        '''
+        Finds and returns the parent of u with respect to the hierarchy.
+        '''
+
+        if self._parent[u] == u:
+            return u
+        else:
+            # Perform path collapse operation
+            self._parent[u] = self.find(self._parent[u])
+            return self._parent[u]
+
+    def merge(self, u, v):
+        '''
+        Merges vertex u into the component of vertex v. Note the
+        asymmetry of this operation.
+        '''
+
+        if u != v:
+            self._parent[self.find(u)] = self.find(v)
+
+    def roots(self):
+        '''
+        Generator expression for returning roots, i.e. components that
+        are their own parents.
+        '''
+
+        for vertex, parent in enumerate(self._parent):
+            if vertex == parent:
+                yield vertex
+
+
+class PersistentHomologyCalculation:
+    def __init__(self, device) -> None:
+        self.device = device
+
+    def __call__(self, matrix):
+
+        n_vertices = matrix.shape[0]
+        uf = UnionFind(n_vertices, self.device)
+
+        triu_indices = np.triu_indices_from(matrix)
+        edge_weights = matrix[triu_indices]
+        edge_indices = th.argsort(edge_weights, stable=True)
+
+        # 1st dimension: 'source' vertex index of edge
+        # 2nd dimension: 'target' vertex index of edge
+        persistence_pairs = []
+
+        for edge_index, edge_weight in \
+                zip(edge_indices, edge_weights[edge_indices]):
+
+            u = triu_indices[0][edge_index]
+            v = triu_indices[1][edge_index]
+
+            younger_component = uf.find(u)
+            older_component = uf.find(v)
+
+            # Not an edge of the MST, so skip it
+            if younger_component == older_component:
+                continue
+            elif younger_component > older_component:
+                uf.merge(v, u)
+            else:
+                uf.merge(u, v)
+
+            if u < v:
+                persistence_pairs.append((u, v))
+            else:
+                persistence_pairs.append((v, u))
+
+        # Return empty cycles component
+        return np.array(persistence_pairs), np.array([])
+
+
+class TopologicalRegularizer(th.nn.Module):
+    """Topologically regularizer."""
+
+    def __init__(self, lam=0.1, device='cpu'):
+        """Topologically Regularized Autoencoder.
+        Args:
+            lam: Regularization strength
+            ae_kwargs: Kewords to pass to `ConvolutionalAutoencoder` class
+            toposig_kwargs: Keywords to pass to `TopologicalSignature` class
+        """
+        super().__init__()
+        self.lam = lam
+        self.device = device
+        self.topo_sig = TopologicalSignatureDistance(self.device)
+
+    @staticmethod
+    def _compute_distance_matrix(x, p=2):
+        x_flat = x.view(x.size(0), -1)
+        distances = th.linalg.vector_norm(x_flat[:, None] - x_flat, dim=2, ord=p)
+        return distances
+
+    def forward(self, models):
+        """Compute the loss of the Topologically regularized autoencoder.
+        Args:
+            x: Input data
+        Returns:
+            Tuple of final_loss, (...loss components...)
+        """
+        model_distances = [model.rnn.weight for model in models]
+        loss = sum([self.topo_sig(model1, model2) for model1, model2 in itertools.combinations(model_distances, 2)])
+        return self.lam * loss
+
+class TopologicalSignatureDistance(th.nn.Module):
+    """Topological signature."""
+
+    def __init__(self, device):
+        """Topological signature computation.
+        """
+        super().__init__()
+        self.device = device
+        self.signature_calculator = PersistentHomologyCalculation(device)
+
+    def _get_pairings(self, distances):
+        pairs_0, pairs_1 = self.signature_calculator(
+            distances)
+
+        return pairs_0, pairs_1
+
+    def _select_distances_from_pairs(self, distance_matrix, pairs):
+        # Split 0th order and 1st order features (edges and cycles)
+        pairs_0, pairs_1 = pairs
+        selected_distances = distance_matrix[(pairs_0[:, 0], pairs_0[:, 1])]
+        return selected_distances
+
+    @staticmethod
+    def sig_error(signature1, signature2):
+        """Compute distance between two topological signatures."""
+        return ((signature1 - signature2)**2).sum(dim=-1)
+
+    # pylint: disable=W0221
+    def forward(self, distances1, distances2):
+        """Return topological distance of two pairwise distance matrices.
+        Args:
+            distances1: Distance matrix in space 1
+            distances2: Distance matrix in space 2
+        Returns:
+            distance, dict(additional outputs)
+        """
+        pairs1 = self._get_pairings(distances1)
+        pairs2 = self._get_pairings(distances2)
+        sig1 = self._select_distances_from_pairs(distances1, pairs1)
+        sig2 = self._select_distances_from_pairs(distances2, pairs2)
+        distance = self.sig_error(sig1, sig2)
+        
+        return distance
